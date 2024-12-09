bf506f028f8101daeef6788f31026666c3f23dcd
CamilaMoi <camilavmoii@gmail.com>
2024-12-09 11:43:36 -0400

    grid

--- Project.swiftpm/ListaLugaresView.swift
+++ Project.swiftpm/ListaLugaresView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ListaLugaresView: View {
+    let columns = [GridItem(.flexible()), GridItem(.flexible())] // Grade com 2 colunas
     
     let lugares: [String] = [
         "Igarapé do 40",
@@ -18,28 +19,39 @@ struct ListaLugaresView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(lugares, id: \.self){ local in
-                    NavigationLink(
-                        destination: LugarView(nome: local),
-                        label: {
-                            HStack {
-                                Image(local)
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                Text(local)
-                                    .multilineTextAlignment(.leading)
-                                    .frame(alignment: .leading)
-                            }
-                        })
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(lugares, id: \.self) { local in
+                        NavigationLink(
+                            destination: LugarView(nome: local),
+                            label: {
+                                VStack {
+                                    Image(local)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 120, height: 120) // Dimensões fixas
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    
+                                    Text(local)
+                                        .font(.headline)
+                                        .foregroundStyle(Color(.gray))
+                                        .multilineTextAlignment(.center)
+                                        .padding(.top, 4)
+                                }
+                                .padding()
+                                .frame(width: 140, height: 180) // Tamanho fixo para todos os itens
+                                .background(Color(.gray).opacity(0.1))
+                                .cornerRadius(20)
+                                .shadow(color: .gray.opacity(0.3), radius: 4, x: 0, y: 2)
+                            })
                     }
+                }
+                .padding()
             }
             .navigationTitle("Lugares")
         }
     }
 }
-
-
 
 #Preview {
     ListaLugaresView()
